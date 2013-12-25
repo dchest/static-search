a8662b965275689b871ea02a505a24d930e742cf
Dmitry Chestnykh <dmitry@codingrobots.com>
2013-12-25 20:04:26 +0100

    Refactor UI code.

--- site/ui-src/search-ui.js
+++ site/ui-src/search-ui.js
@@ -4,15 +4,16 @@
  * https://github.com/dchest/static-search
  */
 
-// <SearchStatus results>
-// results - array of search results.
+// <SearchStatus count>
+// count - number of found results
 //
 var SearchStatus = React.createClass({
   render: function() {
-    var r = this.props.results,
-        msg;
-    if (r && r.length > 0) {
-      msg = 'Found ' + r.length + ' result' + (r.length > 1 ? 's' : '');
+    var plural = function(s, c) {
+      return c == 1 ? s : s + 's';
+    };
+    if (this.props.count > 0) {
+      msg = 'Found ' + this.props.count + plural(' result', this.props.count);
     } else {
       msg = 'No results found';
     }
@@ -20,26 +21,29 @@ var SearchStatus = React.createClass({
   }
 });
 
-// <SearchPagination onNavigate pageNum>
+// <SearchPagination onNavigate pageNum pageCount>
 // onNavigate - function called with page number to navigate to.
 // pageNum - current page number.
+// pageCount - total number of pages.
 //
 var SearchPagination = React.createClass({
   render: function () {
     var props = this.props;
     var createItem = function(text, num) {
-      var className = React.addons.classSet({
+      var classes = React.addons.classSet({
         'active': (num === props.pageNum),
         'disabled': (num < 1 || num > props.pageCount)
       });
-      var tryNavigate = function () {
+      var handleNavigate = function () {
         if (num >= 1 && num <= props.pageCount) {
           props.onNavigate(num);
         }
       };
-      return <li className={className}>
-      <a href="javascript:;" onClick={tryNavigate}>{text}</a>
-      </li>
+      return (
+        <li className={classes}>
+          <a href="javascript:;" onClick={handleNavigate}>{text}</a>
+        </li>
+      );
     };
     var items = [];
     items.push(createItem('«', props.pageNum-1));
@@ -47,10 +51,29 @@ var SearchPagination = React.createClass({
       items.push(createItem(i, i));
     }
     items.push(createItem('»', props.pageNum+1));
-    return <ul className='pagination'>{items}</ul>
+    return <ul className='pagination'>{items}</ul>;
   }
 });
 
+
+// <SearchResult title url>
+// title - title string
+// url - URL string
+//
+var SearchResult = React.createClass({
+  render: function() {
+    return (
+      <li>
+        <div className='search-result-title'>
+          <a href={this.props.url}>{this.props.title || this.props.url}</a>
+        </div>
+        <div className='search-result-url'>
+          {this.props.url}
+        </div>
+      </li>
+    );
+  }
+});
 
 // <SearchResults results>
 // results - array of search results.
@@ -69,34 +92,35 @@ var SearchResults = React.createClass({
     },
 
     render: function() {
-      var allResults = this.props.results || [];
-      var pageCount = Math.ceil(allResults.length / this.props.resultsPerPage);
-      var pageResults = allResults.slice((this.state.pageNum-1)*this.props.resultsPerPage,
-                                         (this.state.pageNum-1)*this.props.resultsPerPage + this.props.resultsPerPage);
+      var results = this.props.results || [];
+      var pageCount = Math.ceil(results.length / this.props.resultsPerPage);
+      var offset = (this.state.pageNum-1) * this.props.resultsPerPage;
 
-      var createResult = function(res) {
-        return <li>
-                 <div className='search-result-title'>
-                   <a href={res.url}>{res.title || res.url}</a>
-                 </div>
-                 <div className='search-result-url'>
-                   {res.url}
-                 </div>
-               </li>
-      }
-      var createPagination = _.bind(function() {
-        if (pageCount > 1) {
-          return <SearchPagination pageNum={this.state.pageNum}
-                                   pageCount={pageCount}
-                                   onNavigate={this.onPageNavigate} />
+      var pageOfResults = _.map(
+        results.slice(offset, offset + this.props.resultsPerPage),
+        function(res) {
+          return <SearchResult title={res.title} url={res.url} />;
         }
-      }, this)
+      );
 
-      return <div className='search-results'>
-               <SearchStatus results={allResults} />
-               <ul>{pageResults.map(createResult)}</ul>
-               {createPagination()}
-             </div>
+      var pagination = null;
+      if (pageCount > 1) {
+        pagination = (
+          <SearchPagination
+            pageNum={this.state.pageNum}
+            pageCount={pageCount}
+            onNavigate={this.onPageNavigate}
+          />
+        );
+      }
+
+      return (
+        <div className='search-results'>
+          <SearchStatus count={results.length} />
+          <ul>{pageOfResults}</ul>
+          {pagination}
+        </div>
+      );
     }
 });
 
@@ -117,29 +141,42 @@ var Search = React.createClass({
   },
 
   render: function() {
-    var searchResults = this.state.query
-                          ? <SearchResults results={this.state.results}
-                                           resultsPerPage={this.props.resultsPerPage} />
-                          : '';
-    return <div>
-              <input type='search'
-                     className='form-control search-query'
-                     placeholder='Search'
-                     value={this.state.query}
-                     onChange={this.onQueryChange} />
-               {searchResults}
-           </div>
+    var searchResults = null;
+    if (this.state.query) {
+       searchResults = (
+         <SearchResults
+           results={this.state.results}
+           resultsPerPage={this.props.resultsPerPage}
+         />
+       );
+    }
+    return (
+      <div>
+        <input
+          type='search'
+          className='form-control search-query'
+          placeholder='Search'
+          value={this.state.query}
+          onChange={this.onQueryChange}
+        />
+        {searchResults}
+      </div>
+    );
   }
 
 });
 
 function StaticSearchUI(elementOrQuery, searcher, resultsPerPage) {
-  _.each(_.isString(elementOrQuery) ?
-           document.querySelectorAll(elementOrQuery)
-         : [ element ], function(el) {
-           React.renderComponent(Search({
-             searcher: searcher,
-             resultsPerPage: resultsPerPage || 10
-           }), el);
-         });
+  var elements;
+  if (_.isString(elementOrQuery)) {
+    elements = document.querySelectorAll(elementOrQuery);
+  } else {
+    elements = [ element ];
+  }
+  _.each(elements, function(e) {
+    React.renderComponent(Search({
+      searcher: searcher,
+      resultsPerPage: resultsPerPage || 10
+    }), e);
+  });
 }
